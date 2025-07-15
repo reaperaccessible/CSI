6dbe3cca5332391ea0db11f756bb620b1837a304
Lee <90565835+reaperaccessible@users.noreply.github.com>
2025-07-15 19:48:02 -0400

    Mise à jour de l'index

---
+++ Main/ReaperAccessible - CSI Edit - Set selection start point of area to remove from selected tracks.lua
@@ -0,0 +1,22 @@
+-- @description Set selection start point of area to remove from selected tracks
+-- @version 1.0
+-- @author Lee JULIEN for ReaperAccessible
+-- @provides [main=main] .
+-- @changelog
+--   # 2024-09-18 - Adding log
+--   # 2024-09-22 - Fixed spoken message
+
+
+-- Sélection temporelle: Définir le point de début
+reaper.Main_OnCommand(40625, 0)
+
+-- Sélectionner les Objets sous le curseur d'édition sur les pistes sélectionnées
+-- SWS: Activer la boucle dans le transport
+local commandId1 = reaper.NamedCommandLookup("_XENAKIOS_SELITEMSUNDEDCURSELTX")
+-- On utilise la fonction habituelle avec la variable commandId
+reaper.Main_OnCommand(commandId1, 0)   
+
+-- Transport: Lecture
+reaper.Main_OnCommand(1007, 0)
+
+reaper.osara_outputMessage("Area start point deleted")
